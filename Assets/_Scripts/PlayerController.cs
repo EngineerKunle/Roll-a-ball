c4e22439caf9ddec369bdb89815ed8ec2a9eaf12
kunle ogunjimi <kogunjimi40@hotmail.com>
2016-11-06 22:37:09 +0000

    started script

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// where our physics code will go in 
+	void FixedUpdate () {
+		//https://unity3d.com/learn/tutorials/projects/roll-ball-tutorial/moving-player?playlist=17141
+		//5min in
+	}
+}
